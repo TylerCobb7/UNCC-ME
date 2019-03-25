6719d1f586038f117082a3f905c5c5d4288c8b93
David Scanlon <dscanlon@uncc.edu>
2019-03-24 21:02:04 -0400

    Removed title bar, added camera permissions to manifest file

--- uncme/uncme/app/src/main/java/com/example/uncme/MainActivity.java
+++ uncme/uncme/app/src/main/java/com/example/uncme/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.uncme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Button button = new Button(R.id.take_video_button);
 
-
+        Toast.makeText(this, "Welcome to UNCme", Toast.LENGTH_LONG).show();
 
     }
 }
